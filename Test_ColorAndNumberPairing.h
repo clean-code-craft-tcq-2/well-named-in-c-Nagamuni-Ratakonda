66e65cc340afc6684b111ceb4d505b04109331ec
Nagamuni-Ratakonda <91056036+Nagamuni-Ratakonda@users.noreply.github.com>
2022-01-18 17:23:19 +0530

    Update Test_ColorAndNumberPairing.h

--- Test_ColorAndNumberPairing.h
+++ Test_ColorAndNumberPairing.h
@@ -1,2 +1,4 @@
+const int MAX_COLORPAIR_NAME_CHARS = 16;
+
 void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
 void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
