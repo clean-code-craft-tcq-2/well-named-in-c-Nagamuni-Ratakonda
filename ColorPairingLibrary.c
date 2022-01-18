c7562cf70f5c5082146839050e42cb178d88bd26
Nagamuni-Ratakonda <91056036+Nagamuni-Ratakonda@users.noreply.github.com>
2022-01-18 17:48:35 +0530

    Update ColorPairingLibrary.c

--- ColorPairingLibrary.c
+++ ColorPairingLibrary.c
@@ -16,7 +16,7 @@ int numberOfMinorColors =
     sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
 void ColorPairToString(const ColorPair* colorPair, char* buffer) {
-    sprintf(buffer, "%s %s",
+    sprintf(buffer, "\t%s\t %s",
         MajorColorNames[colorPair->majorColor],
         MinorColorNames[colorPair->minorColor]);
 }
