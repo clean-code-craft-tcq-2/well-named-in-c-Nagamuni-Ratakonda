9193052838136ae41523abca32dab3d2f58352bb
Nagamuni-Ratakonda <91056036+Nagamuni-Ratakonda@users.noreply.github.com>
2022-01-18 17:09:40 +0530

    Update ColorCodeManual.c

--- ColorCodeManual.c
+++ ColorCodeManual.c
@@ -8,14 +8,14 @@ void PrintColorCode(void)
   ColorPair colorPair;
   
   printf("/***********************************\n");
-  Printf("*********Color Code Manual**********\n");
-  Printf("************************************/\n");
+  printf("*********Color Code Manual**********\n");
+  printf("************************************/\n");
   printf("PAIR NO.\t MAJOR COLOR\t MINOR COLOR\n");
   
   for(PairNum = 1; PairNum <= TotalColorPairs; PairNum++)
   {
     colorPair = GetColorFromPairNumber(PairNum);
-		printf("%d\t%s\t%s\n",PairNum,colorPair.majorColor,colorPair.minorColor);
+    printf("%d\t%d\t%d\n",PairNum,colorPair.majorColor,colorPair.minorColor);
   }
   
   
