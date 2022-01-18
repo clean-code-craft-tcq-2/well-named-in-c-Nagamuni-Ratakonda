bcdc58b1517ba2d9d828a0d83cfd2badc6206826
Nagamuni-Ratakonda <91056036+Nagamuni-Ratakonda@users.noreply.github.com>
2022-01-18 15:58:58 +0530

    Update ColorAndPairingLibrary.h

--- ColorAndPairingLibrary.h
+++ ColorAndPairingLibrary.h
@@ -21,3 +21,8 @@ typedef struct {
     enum MajorColor majorColor;
     enum MinorColor minorColor;
 } ColorPair;
+
+void ColorPairToString(const ColorPair* colorPair, char* buffer);
+ColorPair GetColorFromPairNumber(int pairNumber);
+int GetPairNumberFromColor(const ColorPair* colorPair);
+void PrintColorCode(void);
